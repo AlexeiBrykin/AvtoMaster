c64ce982d3b2a93efe9ae5f76f574753beefb9e0
brykin <alex.engeneer90@gmail.com>
2019-07-19 08:54:03 +0300

    add classes detail and avto

---
+++ AvtoMaster/AvtoMaster/Avto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoMaster
+{
+    class Avto
+    {
+        int idCar = 0;
+        string nameCar;
+        int[] listOfDetails = new int[255];
+    }
+}
---
+++ AvtoMaster/AvtoMaster/Detail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoMaster
+{
+    class Detail
+    {
+        int idDetail = 0;
+        int costDetail = 0;
+        int timeToReplaceDetail = 0;
+        string nameDetail;
+    }
+}
